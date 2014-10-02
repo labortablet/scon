a1494d383ac39b64ad38a94f8cee92d3a7f1179f
FLambda <hg@flambda.de>
2014-10-02 15:22:25 +0200

    Debugging

--- ref_client.py
+++ ref_client.py
@@ -15,16 +15,16 @@ from scon_actions import _uni2bin, _bin2uni
 
 
 pw_db = "test"
-value_db = "$2y$10$wbBPkWPW3dPqgLNR4GlvGeTqf2pWenxSz5pROlX/VMjmz6h1ye/.a"
-salt_db = "wbBPkWPW3dPqgLNR4GlvGg"
+value_db = _uni2bin("$2y$10$wbBPkWPW3dPqgLNR4GlvGeTqf2pWenxSz5pROlX/VMjmz6h1ye/.a")
+salt_db = _uni2bin("wbBPkWPW3dPqgLNR4GlvGg")
 
 url = 'https://lablet.vega.uberspace.de/scon/db.cgi'
 #url = 'https://lablet.vega.uberspace.de/scon/db_ustest.cgi'
 #url = 'https://lablet.vega.uberspace.de/scon/json_bounce.cgi'
 
 def auth_session(session_id, pw, salt, challenge):
-	salt = bcrypt.gensalt(10, _uni2bin(salt))  # modified the bcrypt file to allow my own random bytes
-	challenge = bcrypt.gensalt(10, _uni2bin(challenge))  #modified the bcrypt file to allow my own random bytes
+	salt = bcrypt.gensalt(10, salt)  # modified the bcrypt file to allow my own random bytes
+	challenge = bcrypt.gensalt(10, challenge)  # modified the bcrypt file to allow my own random bytes
 	hash_pw = hashlib.sha256(pw).digest()
 	salted_pw = bcrypt.hashpw(hash_pw, salt)
 	response = bcrypt.hashpw(salted_pw, challenge)
@@ -80,12 +80,12 @@ print("Version:")
 print(get_version())
 tmp = get_challenge("fredi@uni-siegen.de")
 print("Challenge:")
-salt = tmp["salt"]
-challenge = tmp["challenge"]
+salt = _uni2bin(tmp["salt"])
+challenge = _uni2bin(tmp["challenge"])
 session_id = tmp["session_id"]
 print("Challenge: " + challenge)
 print("Session_id: " + session_id)
-print(salt)
+print("Salt: " + salt)
 print(salt_db)
 print("Auth session")
 k = auth_session(session_id, pw_db.encode("utf-8"), salt, challenge)
