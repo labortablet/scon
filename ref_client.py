202e9f000e27892d9602243508693f97b98f2517
FLambda <hg@flambda.de>
2014-10-02 15:25:00 +0200

    Debugging

--- ref_client.py
+++ ref_client.py
@@ -1,7 +1,7 @@
 __author__ = "Frederik Lauber"
 __copyright__ = "Copyright 2014"
 __license__ = "?"
-__version__ = ""
+__version__ = "0.1"
 __maintainer__ = "Frederik Lauber"
 __status__ = "Development"
 __contact__ = "https://flambda.de/impressum.html"
