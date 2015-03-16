6272750b3ea91a147b44fb92af0c8affddf1e50f
FLambda <hg@flambda.de>
2015-03-16 21:35:20 +0100

    Added a pw change dialog

--- new_pw.py
+++ new_pw.py
@@ -10,11 +10,25 @@ import uuid
 import hashlib
 import configparser
 
-from scon_actions import _enable_db
+import pymysql
 
 
-_database = None
-_cursor = None
+def _enable_db(func):
+	global _SERVER_STABLE_RANDOM
+	global _database
+	global _cursor
+	_config = configparser.ConfigParser()
+	_config.read_file(open("/home/lablet/.my.cnf"))
+	_SERVER_STABLE_RANDOM = _config.get('client', 'password')
+	_database = pymysql.connect(unix_socket=_config.get('client', 'socket'),
+	                            port=_config.get('client', 'port'),
+	                            user=_config.get('client', 'user'),
+	                            passwd=_config.get('client', 'password'),
+	                            db="lablet_tabletprojectdb",
+	                            charset='utf8')
+	del _config
+	_cursor = _database.cursor()
+
 
 @_enable_db
 def new_pw(user, password):
@@ -22,8 +36,6 @@ def new_pw(user, password):
 	hash_pw = hashlib.sha256(password).digest()
 	#salted_pw = bcrypt.hashpw(hash_pw, bcrypt.gensalt(10, salt))
 	salted_pw = hashlib.sha256(salt + hash_pw).digest()
-	config = configparser.ConfigParser()
-	config.read_file(open("/home/lablet/.my.cnf"))
 	_cursor.execute("""UPDATE users SET salt = %s, hash_password = %s WHERE email = %s""", (salt, salted_pw, user))
 	_database.commit()
 
